1897eda4857af3eb78d8318303400537711aa116
Martinserrano724 <martinserrano724@gmail.com>
2023-05-26 15:54:13 -0300

    Terminacion de las validaciones

--- src/components/FormularioVeterinaria.jsx
+++ src/components/FormularioVeterinaria.jsx
@@ -3,6 +3,8 @@ import { Form, Button, Card } from "react-bootstrap";
 import { useState, useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import ListaVeterinaria from "./listaVeterinaria";
+import Swal from 'sweetalert2'
+
 const FormularioVeterinaria = () => {
   let citasLocalStorage = JSON.parse(localStorage.getItem("listaCitas")) || [];
 
@@ -15,7 +17,12 @@ const FormularioVeterinaria = () => {
   let veterinaria={nombreMascota:'',nombreDueni:'',fecha:'',hora:'',sintomas:''}
   const handleSubmit = (e) => {
     e.preventDefault();
-    //
+    if (validaciones() == "") {
+      Swal.fire({
+          title: 'Formulario',
+          text: 'Datos Enviados',
+          icon: 'success',
+        })
     veterinaria={nombreMascota,nombreDuenio,fecha,hora,sintomas}
     setArrayCitas([...arrayCitas,veterinaria]);
     setFecha("");
@@ -23,6 +30,14 @@ const FormularioVeterinaria = () => {
     setNombreMascota("");
     setHora("");
     setSintomas("");
+      }
+      else {
+        Swal.fire({
+          title: 'ERROR',
+          text: `${validaciones()}`,
+          icon: 'error',
+        })
+      }
   };
 
   useEffect(() => {
@@ -42,9 +57,36 @@ const FormularioVeterinaria = () => {
     setArrayCitas(citasFiltradas);
   
   };
+  const valicacionTextoInput = (texto) => {
+    if (texto != "" && texto.length < 100) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+  const validaciones = () => {
+    let validacion = "";
+    if (!valicacionTextoInput(nombreMascota)) {
+      validacion += "ERROR Nombre Mascota \n ";
+    }
+    if (!valicacionTextoInput(nombreDuenio)) {
+      validacion += "ERROR Nombre Dueño incorrecto \n ";
+    }
+    if (!fecha>='25/05/2023') {
+      validacion += "ERROR fecha incorrecto \n ";
+    }
+    if (!valicacionTextoInput(sintomas)) {
+      validacion += "ERROR mail incorrecto \n";
+    }
+    if (hora>8 && hora<17){
+      validacion += "ERROR Hora incorrecto \n horario de atencion: de 08 a 17 \n";
+    }
+
+    return validacion;
+  };
 
   return (
-    <div className=" p-5 ps-0 pe-0 container ">
+    <div className=" p-5 ps-0 pe-0  body h-100">
       <h2 className="text-center mb-4">
         Administrador pacientes de veterinaria
       </h2>
@@ -64,6 +106,9 @@ const FormularioVeterinaria = () => {
                     onChange={(e) => setNombreMascota(e.target.value)}
                     value={nombreMascota}
                     className=" w-100"
+                    maxLength={100}
+                    minLength={1}
+                    required
                   />
                 </Form.Group>
                 <Form.Group className="mb-2 ps-3 pe-5 d-flex flex-row">
@@ -76,6 +121,9 @@ const FormularioVeterinaria = () => {
                     onChange={(e) => setNombreDuenio(e.target.value)}
                     value={nombreDuenio}
                     className="w-100"
+                    maxLength={100}
+                    minLength={1}
+                    required
                   />
                 </Form.Group>
                 <Form.Group className="mb-2 ps-3 pe-5  d-flex flex-row  text-center">
@@ -83,22 +131,24 @@ const FormularioVeterinaria = () => {
                     Fecha:
                   </Form.Label>
                   <Form.Control
-                    type="text"
+                    type="date"
                     placeholder="ej: 25/05/2023"
                     onChange={(e) => setFecha(e.target.value)}
                     value={fecha}
-                    className="w-25"
+                    className="w-50"
                   />
 
                   <Form.Label className="col-2 align-self-center">
                     Hora:
                   </Form.Label>
                   <Form.Control
-                    type="text"
+                    type="number"
                     placeholder="hh:mm"
                     onChange={(e) => setHora(e.target.value)}
                     value={hora}
                     className="w-25"
+                    min={8}
+                    max={17}
                   />
                 </Form.Group>
                 <Form.Group className=" ps-3 pe-5  d-flex flex-row">
@@ -110,6 +160,9 @@ const FormularioVeterinaria = () => {
                     placeholder="describir los sintomas"
                     onChange={(e) => setSintomas(e.target.value)}
                     value={sintomas}
+                    maxLength={100}
+                    minLength={1}
+                    required
                   />
                 </Form.Group>
               </Card.Text>
@@ -120,8 +173,10 @@ const FormularioVeterinaria = () => {
           </Card>
         </Form>
       </div>
+      <hr />
       <h3 className="text-center">Administra las citas Aqui</h3>
-      <ListaVeterinaria citas={arrayCitas} eliminar={eliminarCita}></ListaVeterinaria>
+      <div className="">  <ListaVeterinaria citas={arrayCitas} eliminar={eliminarCita}></ListaVeterinaria></div>
+    
     </div>
   );
 };
--- src/components/ListaVeterinaria.jsx
+++ src/components/ListaVeterinaria.jsx
@@ -3,11 +3,13 @@ import CitaVeterinaria from './CitaVeterinaria';
 
 const ListaVeterinaria = ({citas,eliminar}) => {
     return (
-        <div>
-           { citas.map((cita,index)=>{
-                   
-           return <CitaVeterinaria cita={cita} key={index} eliminar={eliminar} index={index}></CitaVeterinaria>})}
+       < div className='container-fluid'>
+        <div className="row d-flex justify-content-evenly align-items-center">
+           { citas.map((cita,index)=>{          
+           return <CitaVeterinaria  className='h-100' cita={cita} key={index} eliminar={eliminar} index={index}></CitaVeterinaria>})}
         </div>
+        </div>
+      
     );
 };
 
