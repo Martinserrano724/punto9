216aecc2a5f7ec0145c0f30fa5244e7cc044a01c
Martinserrano724 <martinserrano724@gmail.com>
2023-05-26 00:36:32 -0300

    Creacion de la rama dev

---
+++ src/components/FormularioVeterinaria.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+const FormularioVeterinaria = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default FormularioVeterinaria;
